135294655e11c48188f657ea82ee52fb1210518d
leejonghyun93 <fhohffodf93@gmail.com>
2025-07-18 20:41:22 +0900

    클라이언트 수정

--- live-streaming-service/src/main/java/org/kosa/livestreamingservice/client/alarm/UserServiceFeignClient.java
+++ live-streaming-service/src/main/java/org/kosa/livestreamingservice/client/alarm/UserServiceFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "${PROD_USER_SERVICE_URL:http://user-service:8103}")
 public interface UserServiceFeignClient {
 
     @GetMapping("/api/users/{userId}/email")
