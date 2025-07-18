bbd31d0c1b5cc0a875dbd10755708f8d71e615cb
leejonghyun93 <fhohffodf93@gmail.com>
2025-07-18 12:39:00 +0900

    클라이언트 수정

--- live-streaming-service/src/main/java/org/kosa/livestreamingservice/client/alarm/UserServiceClient.java
+++ live-streaming-service/src/main/java/org/kosa/livestreamingservice/client/alarm/UserServiceClient.java
@@ -2,6 +2,7 @@ package org.kosa.livestreamingservice.client.alarm;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.List;
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@ConditionalOnBean(UserServiceFeignClient.class)
 public class UserServiceClient {
 
     private final UserServiceFeignClient userServiceFeignClient;
