f06957e7287cedd182e58081f04fd419eaac2c71
leejonghyun93 <fhohffodf93@gmail.com>
2025-07-19 14:27:35 +0900

    상품 서비스 클라이언트 이름변경

--- commerce-service/src/main/java/org/kosa/commerceservice/client/ProductServiceClient.java
+++ commerce-service/src/main/java/org/kosa/commerceservice/client/ProductServiceClient.java
@@ -6,9 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service")
+@FeignClient(name = "commerce-service", url = "${commerce-service.url:http://localhost:8090}")
 public interface ProductServiceClient {
-
     @GetMapping("/api/products/{productId}")
     ProductDTO getProduct(@PathVariable("productId") Integer productId);
 }
